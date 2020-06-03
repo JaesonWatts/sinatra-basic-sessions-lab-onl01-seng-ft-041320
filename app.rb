367b118de17b4bf7d14f3b83a720067c757aca4b
Jaeson Watts <jaeson.watts@gmail.com>
2020-06-03 22:55:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,24 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
-  
+
+
   configure do
-    
+
     enable :sessions
     set :session_secret, SecureRandom.hex(64)
-    
+
   end
-  
+
   get '/' do
     erb :index
   end
-  
+
   post '/checkout' do
     session[:item] = params[:item]
     @session = session
     binding.pry
     erb :checkout
   end
-  
+
 end
